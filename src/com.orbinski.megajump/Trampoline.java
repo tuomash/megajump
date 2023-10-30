2e64b156e366de265d9eb6840c1c7ad9490b8beb
Tuomas Hynninen <tuomas.hynninen@gmail.com>
2023-10-30 12:44:45 +0200

    Remove modifier (not needed)

--- src/com.orbinski.megajump/Trampoline.java
+++ src/com.orbinski.megajump/Trampoline.java
@@ -2,7 +2,7 @@ package com.orbinski.megajump;
 
 class Trampoline extends Entity
 {
-  public Trampoline(final float x, final float y, final float width, final float height)
+  Trampoline(final float x, final float y, final float width, final float height)
   {
     super(x, y, width, height);
   }
