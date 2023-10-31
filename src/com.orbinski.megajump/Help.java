364c2a9876bdf8c37d604178128ebbdf69b22d51
Tuomas Hynninen <tuomas.hynninen@gmail.com>
2023-10-31 15:37:10 +0200

    Update help

--- src/com.orbinski.megajump/Help.java
+++ src/com.orbinski.megajump/Help.java
@@ -11,9 +11,11 @@ class Help
   {
     add("Your objective is to get to the exit door as fast as possible.", 20, 500);
 
-    add("Press N for next level and P for previous level", 20, 120);
-    add("Use WASD or arrow keys to control Orndorf midair", 20, 80);
-    add("Press, drag and release mouse button 1 to launch Orndorf  ", 20, 40);
+    add("Press R to restart the level", 20, 200);
+    add("Press N for next level and P for previous level", 20, 160);
+    add("Use WASD or arrow keys for aerial controls", 20, 120);
+    add("Press, drag and release mouse button 1 to jump", 20, 80);
+    add("Green platforms enable you to jump again", 20, 40);
   }
 
   private void add(final String text, final int x, final int y)
