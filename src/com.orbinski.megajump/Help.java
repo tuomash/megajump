467c5bb44771dc81b28bf7d32c44ca125e8a15b8
Tuomas Hynninen <tuomas.hynninen@gmail.com>
2023-11-06 14:42:16 +0200

    Update help

--- src/com.orbinski.megajump/Help.java
+++ src/com.orbinski.megajump/Help.java
@@ -22,7 +22,8 @@ class Help
 
     // Editor
 
-    addEditor("Press CTRL+E to toggle the level editor on/off", 20, 640);
+    addEditor("Press CTRL+E to toggle the level editor on/off", 20, 680);
+    addEditor("Press CTRL+O to run a custom command. Examples: set gold 10000, set silver 12000, set bronze 15000", 20, 640);
     addEditor("Press CTRL+T to toggle move camera X on/off. Press CTRL+G to toggle move camera Y on/off.", 20, 600);
     addEditor("Press CTRL+Y to raise and CTRL+H to lower death point", 20, 560);
     addEditor("Press CTRL+U to raise and CTRL+J to lower camera floor", 20, 520);
