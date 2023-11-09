d9ee88604242fa59b4b463b822c5f68b8e5f6719
Tuomas Hynninen <tuomas.hynninen@gmail.com>
2023-11-09 17:01:42 +0200

    Remove unused variable

--- src/com.orbinski.megajump/Globals.java
+++ src/com.orbinski.megajump/Globals.java
@@ -9,7 +9,6 @@ class Globals
 
   static final float TIME_STEP_SECONDS = 1 / 60.0f;
   static final float MAX_FRAME_TIME = TIME_STEP_SECONDS * 20.0f;
-  static final int MAX_UPDATES = 20;
 
   static final float GRAVITY = -0.6f;
 
